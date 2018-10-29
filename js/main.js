69c84d636c7708eeef637eb820c58321e82c29c5
mortada gzar <mortadagzar@gmail.com>
2018-10-29 13:52:48 -0700

    vyy

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-var 
+var app=angular.modul('myApp',[]);
+app.controller("myDive", function ($scope){
+$scope.friends=[
+   {hussein:'huhuh',nolan:'noman'} ,
+   {hibba:"habbawi",nader:'nadaqwi'}
+
+
+
+];
+
+
+});
